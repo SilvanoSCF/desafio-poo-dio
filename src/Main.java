4c0d243c9b3721ceddaab9df97ebdb78f734c45a
SilvanoSCF <sscf.prog@gmail.com>
2023-03-02 08:55:38 -0300

    Mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Descrição curso de java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de Logica");
+        curso2.setDescricao("Descrição curso de logica");
+        curso2.setCargaHoraria(12);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria");
+        mentoria.setDescricao("Descricao");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
 
     }
 }
