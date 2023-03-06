ad03cb7c9a6685999f8271604d30761cd2207d6a
SilvanoSCF <sscf.prog@gmail.com>
2023-03-06 00:55:45 -0300

    Progredir

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,5 +1,62 @@
 package br.com.dio.desafio.dominio;
 
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
 public class Dev {
-    // dEV
+
+    private String nome;
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+
+    public void insvreverBootcamp(Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConsteudos());
+    }
+
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+    }
+
+    public void calcularTotalXp(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Dev)) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(getNome(), dev.getNome()) && Objects.equals(getConteudosConcluidos(), dev.getConteudosConcluidos()) && Objects.equals(getConteudosInscritos(), dev.getConteudosInscritos());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome(), getConteudosConcluidos(), getConteudosInscritos());
+    }
 }
